53a10b2840bc7c29dfe5bf878e73b154460969e9
Scott Twiname <skott.twiname@gmail.com>
2020-04-16 15:18:36 +1200

    Revert change in wrong test

--- test/directory.js
+++ test/directory.js
@@ -26,8 +26,6 @@ contract('Directory', accounts => {
 
     const totalStake2 = await directory.getTotalStake();
     assert.equal(totalStake2.toNumber(), 1, "Expected a total stake of 1");
-
-    await directory.addStake(1, accounts[1], { from: accounts[1] });
   });
 
   it('should be able to unstake a leaf', async () => {
