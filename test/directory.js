5044b7b7cb9d664f43fb5bc9c4f76ffcd9fceced
John Carlo San Pedro <j.carlosanpedro@gmail.com>
2021-06-09 15:23:06 +1200

    update some comments in unlockStake implementation and add some tests for scan

--- test/directory.js
+++ test/directory.js
@@ -1,3 +1,7 @@
+const BN = require("bn.js");
+const crypto = require("crypto");
+const sodium = require('libsodium-wrappers-sumo');
+
 const Directory = artifacts.require("Directory");
 const Token = artifacts.require("SyloToken");
 
@@ -63,4 +67,105 @@ contract('Directory', accounts => {
     // Restake
     await directory.addStake(1, accounts[1], { from: accounts[1] });
   });
+
+  it('should distribute scan results amongst stakees proportionally', async () => {
+    for (let i = 0; i < accounts.length; i++) {
+      await directory.addStake(1, accounts[i], { from: accounts[1] });
+      console.log('added stake to', accounts[i]);
+    }
+
+    const results = await collectScanResults(1000);
+    console.log(results);
+  }).timeout(0);
+
+  // it('should be able to scan after unlocking all stake', async () => {
+  //   await directory.addStake(1, accounts[0], { from: accounts[1] });
+  //   await directory.addStake(1, accounts[1], { from: accounts[1] });
+  //   await directory.addStake(1, accounts[2], { from: accounts[1] });
+  // });
+
+  it('should distribute scan results proporitionally after nodes unlock stake', async () => {
+    for (let i = 0; i < accounts.length; i++) {
+      await directory.addStake(1, accounts[i], { from: accounts[1] });
+      console.log('added stake to', accounts[i]);
+    }
+
+    /***
+    *             0
+    *           /   \
+    *          2     1
+    *         / |   | \
+    *        6  4   5  3
+    *            \   \   \
+    *             7   9   8
+    */
+
+    console.log("unstaking for address", accounts[4]);
+    await debug(directory.unlockStake(1, accounts[4], { from: accounts[1] }));
+
+    /***
+    *             0
+    *           /   \
+    *          2     1
+    *         / |   | \
+    *        6  7   5  3
+    *                \  \
+    *                 9   8
+    */
+
+    console.log("unstaking for address", accounts[5]);
+    await directory.unlockStake(1, accounts[5], { from: accounts[1] });
+
+    /***
+    *             0
+    *           /   \
+    *          2     1
+    *         / |   | \
+    *        6  7   9  3
+    *                   \
+    *                    8
+    */
+
+    const results = await collectScanResults(1000);
+    console.log(results);
+
+  }).timeout(0);
 });
+
+async function collectScanResults(iterations) {
+  const points = {};
+  const updatePoint = (address) => {
+    if (!points[address]) {
+      points[address] = 1;
+    } else {
+      points[address]++;
+    }
+  }
+
+  function outputCompletion() {
+    if (i >= itr) {
+      return;
+    }
+    process.stdout.write(" " + (i/itr * 100).toPrecision(2) + "% complete\r")                                                              
+    setTimeout(outputCompletion, 1000);
+  }
+
+  let i = 0;
+
+  outputCompletion();
+
+  console.log("collecting scan results for", iterations, "iterations...");
+
+  while (i < iterations) {
+    const kp = sodium.crypto_sign_keypair('uint8array');
+    const hash = crypto.createHash("sha256");
+    hash.update(kp.publicKey);
+    hash.update(Buffer.from([0]));
+    const point = new BN(hash.digest().subarray(0, 16));
+    const address = await directory.scan(point);
+    updatePoint(address);
+    i++;
+  }
+
+  return points;
+}
--- truffle-config.js
+++ truffle-config.js
@@ -24,4 +24,7 @@ module.exports = {
       }
     }
   },
+  mocha: {
+    enableTimeouts: false
+  }
 };
