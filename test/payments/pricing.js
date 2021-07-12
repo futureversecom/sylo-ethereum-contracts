663cec8b7ab1c4c62490b7fcb0e0f417fcf60f3e
John Carlo San Pedro <j.carlosanpedro@gmail.com>
2021-07-13 11:06:27 +1200

    update tests

--- test/payments/pricing.js
+++ test/payments/pricing.js
@@ -1,45 +1,52 @@
 const BN = require("bn.js");
 const PriceManager = artifacts.require("PriceManager");
+const PriceVoting = artifacts.require("PriceVoting");
+const StakingManager = artifacts.require("StakingManager");
 const Token = artifacts.require("SyloToken");
-const Directory = artifacts.require("Directory");
-const eth = require('eth-lib');
-const { soliditySha3 } = require("web3-utils");
 
 contract('Pricing', accounts => {
   let priceManager;
+  let priceVoting;
   let token;
-  let directory;
+  let stakingManager;
 
   before(async () => {
     token = await Token.new({ from: accounts[1] });
   });
 
   beforeEach(async () => {
-    directory = await Directory.new({ from: accounts[1] });
-    await directory.initialize(token.address, 0, { from: accounts[1] });
+    stakingManager = await StakingManager.new({ from: accounts[1] });
+    await stakingManager.initialize(token.address, 0, { from: accounts[1] });
+
+    priceVoting = await PriceVoting.new({ from: accounts[1] });
+    await priceVoting.initialize(
+      stakingManager.address,
+      { from: accounts[1] }
+    )
 
     priceManager = await PriceManager.new({ from: accounts[1] });
     await priceManager.initialize(
-      directory.address,
+      stakingManager.address,
+      priceVoting.address,
       { from: accounts[1] }
     );
 
-    await token.approve(directory.address, 10000, { from: accounts[1] });
+    await token.approve(stakingManager.address, 10000, { from: accounts[1] });
   });
 
   it('allows a staked account to vote', async () => {
-    await directory.addStake(1, accounts[1], { from: accounts[1] });
-    await priceManager.vote(1, { from: accounts[1] });
+    await stakingManager.addStake(1, accounts[1], { from: accounts[1] });
+    await priceVoting.vote(1, { from: accounts[1] });
   });
 
   it('allows updating a vote', async () => {
-    await directory.addStake(1, accounts[1], { from: accounts[1] });
-    await priceManager.vote(1, { from: accounts[1] });
-    await priceManager.vote(2, { from: accounts[1] });
+    await stakingManager.addStake(1, accounts[1], { from: accounts[1] });
+    await priceVoting.vote(1, { from: accounts[1] });
+    await priceVoting.vote(2, { from: accounts[1] });
   });
 
   it('prevents non staked node from voting', async () => {
-    await priceManager.vote(1, { from: accounts[1] })
+    await priceVoting.vote(1, { from: accounts[1] })
       .then(() => {
         assert.fail('Voting should fail without stake');
       })
@@ -49,8 +56,8 @@ contract('Pricing', accounts => {
   });
 
   it('prevents vote with value set to 0', async () => {
-    await directory.addStake(1, accounts[1], { from: accounts[1] });
-    await priceManager.vote(0, { from: accounts[1] })
+    await stakingManager.addStake(1, accounts[1], { from: accounts[1] });
+    await priceVoting.vote(0, { from: accounts[1] })
       .then(() => {
         assert.fail('Voting should fail if price is 0');
       })
@@ -61,16 +68,13 @@ contract('Pricing', accounts => {
 
   it('can determince price after votes and staking has occured', async () => {
     for (let i = 0; i < accounts.length; i++) {
-      await directory.addStake(10, accounts[i],{ from: accounts[1] });
+      await stakingManager.addStake(10, accounts[i],{ from: accounts[1] });
     }
-
-    const stake = await directory.getTotalStake({ from: accounts[1] });
 
     const prices = [20, 10, 5, 30, 45, 12, 17, 9, 24, 10];
     
-
     for (let i = 0; i < accounts.length; i++) {
-      await priceManager.vote(prices[i], { from: accounts[i] });
+      await priceVoting.vote(prices[i], { from: accounts[i] });
     }
 
     let sortedVotes = [];
@@ -79,9 +83,9 @@ contract('Pricing', accounts => {
     }
     sortedVotes.sort((a, b) => a.price - b.price);
 
-    await priceManager.calculatePrice(sortedVotes, { from: accounts[1] });
+    await priceManager.calculatePrices(sortedVotes, { from: accounts[1] });
 
-    const currentPrice = await priceManager.currentPrice();
+    const currentPrice = await priceManager.currentServicePrice();
 
     // Each stake is 10, and the total stake is 100. The lower boundary is 25,
     // so the boundary will be crossed when getting to the third sorted vote
---
+++ test/staking.js
@@ -0,0 +1,199 @@
+const BN = require("bn.js");
+const crypto = require("crypto");
+const sodium = require('libsodium-wrappers-sumo');
+
+const Directory = artifacts.require("Directory");
+const StakingManager = artifacts.require("StakingManager");
+const PriceManager = artifacts.require("PriceManager");
+const PriceVoting = artifacts.require("PriceVoting");
+const Token = artifacts.require("SyloToken");
+
+contract('Staking', accounts => {
+  let token;
+  let stakingManager;
+  let priceManager;
+  let priceVoting;
+  let directory;
+
+  before(async () => {
+    token = await Token.new({ from: accounts[1] });
+  });
+
+  beforeEach(async () => {
+    stakingManager = await StakingManager.new({ from: accounts[1] });
+    await stakingManager.initialize(token.address, 0, { from: accounts[1] });
+
+    priceVoting = await PriceVoting.new({ from: accounts[1] });
+    await priceVoting.initialize(
+      stakingManager.address,
+      { from: accounts[1] }
+    )
+
+    priceManager = await PriceManager.new({ from: accounts[1] });
+    await priceManager.initialize(
+      stakingManager.address,
+      priceVoting.address,
+      { from: accounts[1] }
+    );
+
+    await token.approve(stakingManager.address, 10000, { from: accounts[1] });
+
+    directory = await Directory.new({ from: accounts[1] });
+    await directory.initialize(
+        priceVoting.address, 
+        priceManager.address, 
+        stakingManager.address, 
+      { from: accounts[1] }
+    );
+  });
+
+  it('should be able to restake when everything is unstaked', async () => {
+    await stakingManager.addStake(1, accounts[1], { from: accounts[1] });
+
+    await stakingManager.unlockStake(1, accounts[1], { from: accounts[1] });
+
+    // Restake
+    await stakingManager.addStake(1, accounts[1], { from: accounts[1] });
+  });
+
+  it('should distribute scan results amongst stakees proportionally - all equal', async () => {
+    for (let i = 0; i < accounts.length; i++) {
+      await stakingManager.addStake(1, accounts[i], { from: accounts[1] });
+    }
+
+    await voteAndConstructDirectory();
+
+    let expectedResults = {}
+    for (let i = 0; i < accounts.length; i++) {
+      expectedResults[accounts[i]] = 1/10 * 1000;
+    }
+
+    const results = await collectScanResults(1000);
+    for (let key of Object.keys(expectedResults)) {
+      const expected = expectedResults[key];
+      const actual = results[key];
+      console.log('For address', key, 'expected=', expected, 'actual=', actual);
+    }
+  }).timeout(0);
+
+  it('should distribute scan results amongst stakees proportionally - varied stake amounts', async () => {
+    let totalStake = 0;
+    for (let i = 0; i < accounts.length; i++) {
+      await stakingManager.addStake(i + 1, accounts[i], { from: accounts[1] });
+      totalStake += i + 1;
+    }
+
+    await voteAndConstructDirectory();
+
+    let expectedResults = {}
+    for (let i = 0; i < accounts.length; i++) {
+      expectedResults[accounts[i]] = parseInt((i+1)/totalStake * 1000);
+    }
+
+    const results = await collectScanResults(1000);
+    for (let key of Object.keys(expectedResults)) {
+      const expected = expectedResults[key];
+      const actual = results[key];
+      console.log('For address', key, 'expected=', expected, 'actual=', actual);
+    }
+  }).timeout(0);
+
+  it('should be able to scan after unlocking all stake', async () => {
+    await stakingManager.addStake(1, accounts[0], { from: accounts[1] });
+    await stakingManager.addStake(1, accounts[1], { from: accounts[1] });
+    await stakingManager.addStake(1, accounts[2], { from: accounts[1] });
+
+    await stakingManager.unlockStake(1, accounts[0], { from: accounts[1] });
+    await stakingManager.unlockStake(1, accounts[1], { from: accounts[1] });
+    await stakingManager.unlockStake(1, accounts[2], { from: accounts[1] });
+
+    const address = await directory.scan(0);
+
+    assert.equal(address, '0x0000000000000000000000000000000000000000', "Expected zero address");
+  });
+
+  it('scan after some nodes unstake', async () => {
+    for (let i = 0; i < accounts.length; i++) {
+      await stakingManager.addStake(1, accounts[i], { from: accounts[1] });
+    }
+
+    for (let i = 5; i < accounts.length; i++) {
+      await stakingManager.unlockStake(1, accounts[i], { from: accounts[1] });
+    }
+
+    let sortedVotes = [];
+    for (let i = 0; i < 5; i++) {
+      await priceVoting.vote(1, { from: accounts[i] });
+      sortedVotes.push({ voter: accounts[i], price: 1 });
+    }
+    await priceManager.calculatePrices(sortedVotes, { from: accounts[1] });
+    await directory.constructDirectory({ from: accounts[1] });
+
+    let expectedResults = {}
+    for (let i = 1; i < 5; i++) {
+      expectedResults[accounts[i]] = parseInt(1/5 * 1000);
+    }
+
+    const results = await collectScanResults(1000);
+    for (let key of Object.keys(expectedResults)) {
+      const expected = expectedResults[key];
+      const actual = results[key];
+      console.log('For address', key, 'expected=', expected, 'actual=', actual);
+    }
+  }).timeout(0);
+
+  async function voteAndConstructDirectory() {
+    let sortedVotes = [];
+    for (let i = 0; i < accounts.length; i++) {
+      await priceVoting.vote(1, { from: accounts[i] });
+      sortedVotes.push({ voter: accounts[i], price: 1 });
+    }
+    sortedVotes.sort((a, b) => a.price - b.price);
+    await priceManager.calculatePrices(sortedVotes, { from: accounts[1] });
+    await directory.constructDirectory({ from: accounts[1] });
+  }
+
+  async function collectScanResults(iterations) {
+    const points = {};
+    const updatePoint = (address) => {
+      if (!points[address]) {
+        points[address] = 1;
+      } else {
+        points[address]++;
+      }
+    }
+  
+    function outputCompletion() {
+      if (i >= iterations) {
+        return;
+      }
+      process.stdout.write(" " + (i/iterations * 100).toPrecision(2) + "% completed\r")                                                              
+      setTimeout(outputCompletion, 1000);
+    }
+  
+    let i = 0;
+  
+    outputCompletion();
+  
+    console.log("collecting scan results for", iterations, "iterations...");
+  
+    while (i < iterations) {
+      // generate a random ed25519 key and hash with an epoch to create a
+      // 'random' point value
+      const kp = sodium.crypto_sign_keypair('uint8array');
+      const hash = crypto.createHash("sha256");
+      hash.update(kp.publicKey);
+      hash.update(Buffer.from([0])); // append epoch
+      const point = new BN(hash.digest().subarray(0, 16));
+      const address = await directory.scan(point);
+      updatePoint(address);
+      i++;
+    }
+  
+    return points;
+  }
+
+  async function stakeAndVote(address, stakeAmount) {
+
+  } 
+});
