fd5da08ef57f9368046bbf9e8acc3b2d32cfc246
John Carlo San Pedro <j.carlosanpedro@gmail.com>
2021-08-09 13:36:17 +1200

    remove unused abdk package

--- contracts/Utils.sol
+++ contracts/Utils.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "abdk-libraries-solidity/ABDKMathQuad.sol";
-
 library SyloUtils {
     uint256 constant PERCENTEGE_DENOMINATOR = 100;
 
