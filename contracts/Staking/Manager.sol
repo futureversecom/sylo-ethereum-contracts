05364c49eda67d1abc47b3614b34cf8c3fb38c0c
Josiah Bull <josiah.bull7@gmail.com>
2021-11-16 14:52:42 +1300

    fix: Removed an extraneous import and trailing whitespace.

--- contracts/Staking/Manager.sol
+++ contracts/Staking/Manager.sol
@@ -8,7 +8,6 @@ import "../Token.sol";
 import "../Payments/Ticketing/RewardsManager.sol";
 import "../Epochs/Manager.sol";
 import "../Utils.sol";
-import "abdk-libraries-solidity/ABDKMath64x64.sol";
 
 /**
  * @notice Manages stakes and delegated stakes for Nodes. Holding
@@ -253,7 +252,7 @@ contract StakingManager is Initializable, OwnableUpgradeable {
      * of unlocking. As this essentially adds back stake to the Node, this
      * will trigger an automatic claim of any outstanding staking rewards.
      * If the specified amount to cancel is greater than the stake that is
-     * currently being staked, it will cancel the maximum stake possible.
+     * currently being unlocked, it will cancel the maximum stake possible.
      * @param amount The amount of unlocking stake to cancel in SOLO.
      * @param stakee The address of the staked Node.
      */
@@ -269,7 +268,7 @@ contract StakingManager is Initializable, OwnableUpgradeable {
             unlock.amount -= amount;    
         }   
 
-        addStake_(amount, stakee);     
+        addStake_(amount, stakee);
     }
 
     /**
