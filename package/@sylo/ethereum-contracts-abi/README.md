d11e835ef60d4a23f131d1d459620854ae54ba55
John Carlo San Pedro <j.carlosanpedro@gmail.com>
2021-12-09 12:16:21 +1300

    create package for abis

---
+++ package/@sylo/ethereum-contracts-abi/README.md
@@ -0,0 +1,5 @@
+Contains the compiled ABI's for the Sylo Ethereum contracts.
+
+These ABI's can be used to generate typechain bindings,
+and in conjunction with various ethereum clients such as
+ethers, web3, and etc.
---
+++ scripts/packageAbis.js
@@ -0,0 +1,57 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const PKG_DIR = path.resolve(__dirname, "../package/@sylo/ethereum-contracts-abi");
+const ABI_DIR = path.resolve(__dirname, "../abi");
+
+const syloContracts = [
+  "Directory",
+  "EpochsManager",
+  "Listings",
+  "RewardsManager",
+  "StakingManager",
+  "SyloTicketing",
+  "SyloToken",
+  "TicketingParameters"
+]
+
+async function parseAbis() {
+  const abis = await Promise.all(syloContracts.map(async (contract) => {
+    const src = `${ABI_DIR}/${contract}.abi`;
+    const abi = await fs.readFile(src)
+      .then(b => b.toString());
+
+    await fs.copyFile(src, `${PKG_DIR}/${contract}.abi`);
+
+    return { contract, abi };
+  }));
+
+  return abis;
+}
+
+async function writePackageIndex(abis) {
+  let f = ``;
+
+  abis.map(a => {
+    f += `const ${a.contract} = ${a.abi};\n`;
+  });
+
+  f += '\n';
+  f += 'module.exports = {\n';
+
+  abis.map((a, i) => {
+    f += `  ${a.contract}: ${a.contract}${i < abis.length - 1 ? ',' : ''}\n`;
+  })
+
+  f += '}';
+
+  await fs.writeFile(`${PKG_DIR}/index.js`, f);
+}
+
+
+async function main() {
+  const abis = await parseAbis();
+  await writePackageIndex(abis);
+}
+
+main();
