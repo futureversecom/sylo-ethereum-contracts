cfc62d8a9cd49ca094bfe0e29bb2a2d965849df9
Taylor <phuongthao234567@gmail.com>
2022-03-17 13:11:48 +1300

    Add check full coverage

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,30 @@
+const fs = require('fs')
+
+const checkCoverage = () => {
+  let contractStr = fs.readFileSync('coverage.json')
+  let contracts = JSON.parse(contractStr)
+
+  for (const [_, contract] of Object.entries(contracts)) {
+    const { l, s, b, f } = contract
+    const results = [ l, s, b, f ]
+
+    for (const [_, valObj] of Object.entries(results)) {
+      for (const [_, val] of Object.entries(valObj)) {
+        if (Array.isArray(val)) {
+          val.map(v => v === 0 && process.exit(1) )
+        } else if (val === 0) {
+          process.exit(1)
+        }
+      }
+    }
+  }
+}
+
 module.exports = {
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
     invert: true               // Run the grep's inverse set.
   },
-  skipFiles: ['ECDSA.sol']
+  skipFiles: ['ECDSA.sol'],
+  onIstanbulComplete: checkCoverage
 }
