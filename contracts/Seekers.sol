e6e23a34a664f55acac31ff07674814e47ae9fc6
John Carlo San Pedro <j.carlosanpedro@gmail.com>
2022-05-23 15:53:07 +1200

    implement contract for managing state oracle reads for seekers

---
+++ contracts/Seekers.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Seekers is Initializable, OwnableUpgradeable {
+
+    struct Owner {
+        address owner;
+        uint256 expiry;
+    }
+
+    mapping (uint256 => uint256) public requests;
+    mapping (uint256 => Owner) private owners;
+
+    /**
+     * @notice The address of the Seekers NFT contract on ethereum mainnet.
+     */
+    address public seekers;
+
+    /**
+     * @notice The address of the Oracle account performing these attestations.
+     */
+    address public oracle;
+
+    /**
+     * @notice The duration in blocks a response from the State Oracle
+     * will be valid for. Any queries for ownership of a seeker that is older
+     * than this value will return address(0).
+     */
+    uint256 public validDuration;
+
+    /**
+     * @notice The maximum gas the callback function should take to execute.
+     */
+    uint256 public callbackGasLimit;
+
+    /**
+     * @notice The amount in CPAY relayers will be rewarded for performing
+     * the state bridge request.
+     */
+    uint256 public callbackBounty;
+
+    function initialize(
+        address _seekers,
+        address _oracle,
+        uint256 _validDuration,
+        uint256 _callbackGasLimit,
+        uint256 _callbackBounty
+    ) external initializer {
+        seekers = _seekers;
+        oracle = _oracle;
+        validDuration = _validDuration;
+        callbackGasLimit = _callbackGasLimit;
+        callbackBounty = _callbackBounty;
+    }
+
+    function setSeekers(address _seekers) external onlyOwner {
+        seekers = _seekers;
+    }
+
+    function setOracle(address _oracle) external onlyOwner {
+        oracle = _oracle;
+    }
+
+    function setValidDuration(uint256 _validDuration) external onlyOwner {
+        validDuration = _validDuration;
+    }
+
+    function setCallbackGasLimit (uint256 _callbackGasLimit) external onlyOwner {
+        callbackGasLimit = _callbackGasLimit;
+    }
+
+    function setCallbackBounty (uint256 _callbackBounty) external onlyOwner {
+        callbackBounty = _callbackBounty;
+    }
+
+    function requestVerification(uint256 seekerId) external payable {
+        bytes memory ownerOfCall = abi.encodeWithSignature("ownerOf(uint256)", seekerId);
+        bytes4 callbackSelector = this.confirmOwnership.selector;
+
+        // request a remote eth_call via the state oracle
+        bytes memory remoteCallRequest = abi.encodeWithSignature(
+            "remoteCall(address,bytes,bytes4,uint256,uint256)",
+            seekers,
+            ownerOfCall,
+            callbackSelector,
+            callbackGasLimit,
+            callbackBounty
+        );
+
+        (bool success, bytes memory returnData) = oracle.call(remoteCallRequest);
+        require(success);
+
+        uint256 requestId = abi.decode(returnData, (uint256));
+
+        requests[requestId] = seekerId;
+    }
+
+
+    function confirmOwnership(uint256 requestId, bytes32 returnData) external {
+        require(msg.sender == oracle, "must be state oracle");
+
+        address owner = address(uint160(uint256(returnData)));
+
+        uint256 seekerId = requests[requestId];
+
+        owners[seekerId] = Owner(owner, block.number + validDuration);
+
+        delete requests[requestId];
+    }
+
+    function ownerOf(uint256 seekerId) external view returns (address) {
+        Owner memory owner = owners[seekerId];
+
+        if (owner.owner == address(0)) {
+            return address(0);
+        }
+
+        // State Oracle's response has become stale.
+        if (owner.expiry < block.number) {
+            return address(0);
+        }
+
+        return owner.owner;
+    }
+
+    function withdrawAll(address recipient) public onlyOwner {
+        uint256 balance = address(this).balance;
+        payable(recipient).transfer(balance);
+    }
+
+    function withdrawAllViaCall(address payable _to) public onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool sent, bytes memory data) = _to.call{value: balance}("");
+        require(sent, "Failed to send Ether");
+    }
+}
